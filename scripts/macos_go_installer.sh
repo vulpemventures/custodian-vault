f455ba05314f78eca5e96136ab6f221c6a5155f4
altafan <pietro@vulpem.com>
2018-09-21 02:04:53 +0200

    macos scripts #10

---
+++ scripts/macos_go_installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+brew uninstall go
+brew update
+brew install golang
+
+mkdir -p $HOME/go/bin $HOME/go/src $HOME/go/pkg
+
+echo "export GOPATH=$HOME/go"
+echo "export GOROOT=/usr/local/opt/go/libexec"
+echo "export PATH=$PATH:$GOPATH/bin"
+echo "export PATH=$PATH:$GOROOT/bin"
+
+echo -e "Installation complete\nGo installed at path: /usr/local/go\nWorkspace set at GOPATH=$GOPATH"
---
+++ scripts/macos_vault_installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ARCH=`uname -m`
+TARGET="vault_0.11.1_darwin_"
+case "$ARCH" in
+  "x86")
+    TARGET="386"
+    ;;
+  "x86_64")
+    TARGET="amd64"
+    ;;
+  *)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
+TARGET=$TARGET".zip"
+
+echo "Downloading archive.."
+curl -O https://releases.hashicorp.com/vault/0.11.1/$TARGET
+
+rm -rf $HOME/vault
+mkdir -p $HOME/vault
+echo "Extracting files.."
+unzip $TARGET -d $HOME/vault
+rm -rf $TARGET
+echo "export PATH=\$PATH:$HOME/vault" >> $HOME/.bash_profile
+echo -e "Installation complete\nVault installed at path: $HOME/vault"
